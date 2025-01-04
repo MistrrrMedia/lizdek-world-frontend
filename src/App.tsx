93b892e96d5198182d0d953974c677a94e2cab18
MistrrrMedia <65423117+MistrrrMedia@users.noreply.github.com>
2025-01-04 14:58:04 -0600

    Added Social Icons, Fixed Title

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App() {
                 loop
                 muted
                 playsInline
-                className="background-loop"
+                className="background_loop"
             />
 
-            <div>
+            <div className="content_container">
                 <img
                     src="/lizdekintro.gif"
                     alt="lizdek_intro"
-                    className="lizdek-gif"
+                    className="lizdek_gif"
                 />
 
                 <div>
@@ -25,8 +25,22 @@ function App() {
                     <a href="mailto:tiff@24-8.com">CONTACT</a>
                 </div>
             </div>
+
+            <div className="social_container">
+                <a className="social_link" href="https://instagram.com/lizdekmusic" target="_blank" rel="noopener noreferrer">
+                    <img src="/instagram.svg" alt="instagram" className="social_icon"/>
+                </a>
+
+                <a className="social_link" href="https://tiktok.com/@lizdekmusic" target="_blank" rel="noopener noreferrer">
+                    <img src="/tik-tok.svg" alt="tiktok" className="social_icon"/>
+                </a>
+
+                <a className="social_link" href="https://twitter.com/lizdekmusic" target="_blank" rel="noopener noreferrer">
+                    <img src="/twitter.svg" alt="twitter" className="social_icon"/>
+                </a>
+            </div>
         </>
     )
 }
 
-export default App
+export default App;
