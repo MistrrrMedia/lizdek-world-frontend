dc3a42ff01c49718b075fff3fe586e27236ba943
MistrrrMedia <65423117+MistrrrMedia@users.noreply.github.com>
2025-08-03 23:29:47 -0500

    fix: this is why we run shit before committing
    - removed link

--- src/pages/Releases.tsx
+++ src/pages/Releases.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
 import { useReleases } from '../hooks/useReleases';
 import { getAlbumArtURL } from '../services/artworkRetriever';
 import '../styles/Releases.css';
